947537ea3fe71e8de351281b27be925817f84057
liyihai-official <pokemonarrive@gmail.com>
2024-05-27 23:11:14 +0100

    Update

--- MPI.Heat/C++.Heat/NEW/benchmark_first.sh
+++ MPI.Heat/C++.Heat/NEW/benchmark_first.sh
@@ -19,17 +19,15 @@ if [ ! -d "benchmark.results.25.5.first" ]; then
     mkdir benchmark.results.25.5.first
 fi
 
-# 1   2   4   8  16  32    64
-# 1   2   4   8  16  32    64
 
-PROCS=(       128 256)
+PROCS=(      1   2   4   8  16  32    64 128 256)
 SIZE_N_X=1024
 SIZE_N_Y=1024
 
 echo ""
 echo "Strong Scaling NO OMP"
 echo ""
-for idx in 0 1 2 ; do
+for idx in 0 1 2 3 4 5 6 7 8 9; do
   PROC=${PROCS[idx]}
   echo "Running with ${PROC} process Grid size ${SIZE_N_X}*${SIZE_N_Y}"
   make clean
@@ -39,13 +37,13 @@ for idx in 0 1 2 ; do
 done
 
 
-PROCS=(        128  256)
-SIZE_N_XS=( 1024 2048) # 128 128 256 256 512 512  1024
-SIZE_N_YS=( 2048 2048) # 128 256 256 512 512 1024 1024 
+PROCS=(      1   2   4   8  16  32    64  128  256)
+SIZE_N_XS=(128 128 256 256 512 512  1024 1024 2048)
+SIZE_N_YS=(128 256 256 512 512 1024 1024 2048 2048)
 
 echo "Weak Scaling NO OMP"
 echo ""
-for idx in 0 1 2 ; do
+for idx in 0 1 2 3 4 5 6 7 8 9; do
   PROC=${PROCS[idx]}
   SIZE_N_X=$((SIZE_N_XS[idx]))
   SIZE_N_Y=$((SIZE_N_YS[idx]))
