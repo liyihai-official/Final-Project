fb3e49596a6a3685a4f107ad7149f5b9256e2028
Parley <47943866+liyihai-official@users.noreply.github.com>
2024-06-13 20:42:16 +0800

    Update

--- PIDNN/C++.Heat/main.cc
+++ PIDNN/C++.Heat/main.cc
@@ -1,37 +1,71 @@
 #include <iostream>
-#include <memory>
 
 #include "neuron.cpp"
+#include "../../MPI.Heat/C++.Heat/ver3.0/multi_array/array_distribute.cpp"
+#include "../../MPI.Heat/C++.Heat/ver3.0/multi_array/array_io.cpp"
 
 int main ( int argc, char ** argv)
 {
 
-  std::unique_ptr<Neuron> N1, N2, N3;
+  std::unique_ptr<NeuralNetwork::Neuron> N1, N2, N3;
 
-  N1 = std::make_unique<Neuron>(0.2);
-  N2 = std::make_unique<Neuron>(0.3);
-  N3 = std::make_unique<Neuron>(0.4);
+  N1 = std::make_unique<NeuralNetwork::Neuron>(0.2);
+  N2 = std::make_unique<NeuralNetwork::Neuron>(0.3);
+  N3 = std::make_unique<NeuralNetwork::Neuron>(0.4);
 
 
-  std::cout 
-  << "Value : " << N1->getVal() 
+  std::cout
+  << " Value : " << N1->getVal() 
   << " Activate Value : " << N1->get_activated_value() 
   << " Derivate " << N1->get_derivative_Value() << std::endl;
 
   std::cout 
-  << "Value : " << N2->getVal() 
+  << " Value : " << N2->getVal() 
   << " Activate Value : " << N2->get_activated_value() 
   << " Derivate " << N2->get_derivative_Value() << std::endl;
 
   std::cout 
-  << "Value : " << N3->getVal() 
+  << " Value : " << N3->getVal() 
   << " Activate Value : " << N3->get_activated_value() 
   << " Derivate " << N3->get_derivative_Value() << std::endl;
 
   
 
+  auto Layer {NeuralNetwork::Layer(3)};
 
 
+
+  auto A = final_project::array2d<double>(3,3);
+  std::cout << " ------------------------------------- \n";
+  A.fill_random();
+
+  std::cout << A << std::endl;
+
+  A.transpose();
+  
+  std::cout << A << std::endl;
+
+
+
+  std::cout << " ------------------------------------- \n";
+  std::vector<std::size_t> topology {3, 2, 3};
+
+  // for (auto const & t : topology) { std::cout << t << " "; }
+  // std::cout << "\n";
+
+  NeuralNetwork::Network nn (topology);
+
+  nn.show();
+
+  std::cout << " ------------------------------------- \n";
+  auto input = final_project::array2d<double>(1,3);
+  input.fill_random();
+  std::cout << input << std::endl;
+
+  std::cout << " ------------------------------------- \n";
+  nn.front_propagation(input);
+
+  
 
 
 
--- PIDNN/C++.Heat/neuron.cpp
+++ PIDNN/C++.Heat/neuron.cpp
@@ -2,12 +2,19 @@
 #define _NEURON_CPP_
 
 #pragma once 
-#include <iostream>
-#include <cmath>
+#include <memory>
+#include <random>
+#include <functional>
+
+#include "../../MPI.Heat/C++.Heat/ver3.0/multi_array/base.cpp"
+
+namespace NeuralNetwork
+{
 
 class Neuron
 {
   public:
+  Neuron();
   Neuron(double value);
 
   void activate();
@@ -21,25 +28,154 @@ class Neuron
 
   private:
   double value, activateValue, derivativeValue;
+}; // class Neuron
+
+class NeuronMats
+{
+  public:
+  NeuronMats();
+  NeuronMats(std::size_t Rows, std::size_t Cols);
+  
+  private:
+  final_project::array2d<double> MatValue, MatActivateValue, MatDerivativeValue;
+}; // class Neuron Matrices
+
+
+class Layer
+{
+
+  public:
+  Layer();
+  Layer(std::size_t size);
+
+  private:
+  std::size_t               size;
+  std::unique_ptr<Neuron[]> Neurons;
+}; // class Layer
+
+
+
+class Network
+{
+  public:
+  Network(std::vector<std::size_t> & topology);
+
+  private:
+  std::vector<std::size_t> topology;
+
+  private:
+  std::unique_ptr<Layer[]> Layers;
+  std::unique_ptr<final_project::array2d<double>[]> weightMatrices;
+
+  public:
+  void show()
+  {
+    for (std::size_t i = 0; i < topology.size() - 1; ++i)
+    {
+      std::cout << weightMatrices[i] << std::endl;
+    }
+  }
+
+  public:
+  void front_propagation(final_project::array2d<double> & input)
+  {
+FINAL_PROJECT_ASSERT_MSG(input.Cols == weightMatrices[0].Rows, "Invalid shape of input sample.");
+    for (std::size_t i = 0; i < topology.size() - 1; ++i)
+    {
+      input = input * weightMatrices[i];
+    }
+    std::cout << input << std::endl;
+  }
 };
 
+} // namespace NeuralNetwork
 
-Neuron::Neuron(double value)
-: value {value} 
+
+
+
+/// Member Functions
+
+#include <cmath>
+#include <iostream>
+#include <ranges>
+
+
+namespace NeuralNetwork
 {
+
+// ---------------------------------------------------------------------------
+// Class Neuron Member functions
+
+/// @brief Default Constructor of Neuron Ojbect
+/// @param value 
+Neuron::Neuron()
+: value {0} {
   activate();
   derivative();
 }
 
-void Neuron::activate()
-{
-  activateValue = value / ( 1 + std::abs(value) ) ;
+/// @brief Constructor of Neuron Ojbect
+/// @param value 
+Neuron::Neuron(double value)
+: value {value} {
+  activate();
+  derivative();
 }
 
-void Neuron::derivative()
-{
-  derivativeValue = value * ( 1 - value );
+/// @brief Get the activated value
+void Neuron::activate()
+  { activateValue = value / ( 1 + std::abs(value) ) ; }
+
+/// @brief Get the derivative of input value
+void Neuron::derivative() 
+  { derivativeValue = value * ( 1 - value ); }
+
+// ---------------------------------------------------------------------------
+// Class Neuron Matrices Member Functions
+// NeuronMats::NeuronMats(std::size_t Rows, std::size_t Cols)
+// {
+
+// }
+
+
+
+// ---------------------------------------------------------------------------
+// Class Layer Member functions 
+
+Layer::Layer()
+: size {0}, Neurons {std::make_unique<Neuron[]>(0)} { }
+
+/// @brief Constructor
+/// @param size The number of neurons in this layer.
+Layer::Layer(std::size_t size) 
+: size {size}, Neurons {std::make_unique<Neuron[]>(size)} {
+  for (std::size_t i = 0; i < size; ++i) { Neurons[i] = Neuron(0.0); }
+
 }
+
+
+// ---------------------------------------------------------------------------
+// Class NeuralNetwork member functions
+Network::Network(std::vector<std::size_t> & topology)
+: topology {topology}
+{
+  Layers = std::make_unique<Layer[]>(topology.size());
+  weightMatrices = std::make_unique<final_project::array2d<double>[]>(topology.size()  - 1);
+
+  for (std::size_t i = 0; i < topology.size(); ++i) { Layers[i] = Layer(topology[i]); }
+
+  for (std::size_t i = 0; i < topology.size() - 1; ++i ) { 
+    weightMatrices[i] = final_project::array2d<double>(topology[i], topology[i+1]); 
+    weightMatrices[i].fill_random();
+  }
+    
+}
+
+} // namespace NeuralNetwork
+
+
+
+
 
 
 #endif /// _NEURON_CPP_
