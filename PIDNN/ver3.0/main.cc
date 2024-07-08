97030946be9f1c6fa2e275a6c3d0fbc4dc1a6dc8
liyihai-official <pokemonarrive@gmail.com>
2024-07-09 00:07:48 +0800

    Update

--- PIDNN/ver3.0/main.cc
+++ PIDNN/ver3.0/main.cc
@@ -1,7 +1,35 @@
 #include <torch/torch.h>
 #include <iostream>
+#include <chrono>
+
+struct CustomLoss : torch::nn::Module {
+    torch::Tensor forward(  const torch::Tensor & output, 
+                            const torch::Tensor & target)
+    {return torch::mse_loss(output, target) + 0.01 * torch::sum(torch::abs(output));}
+
+}; // end strct Custom loss : torch::nn::Module
 
 
+struct Net : torch::nn::Module {
+    Net() 
+    {
+        fc1 = register_module("fc1", torch::nn::Linear(10, 500));
+        fc2 = register_module("fc2", torch::nn::Linear(500, 500));
+        fc3 = register_module("fc3", torch::nn::Linear(500, 1));
+    }
+
+    torch::Tensor forward(torch::Tensor x)
+    {
+        x = torch::relu(fc1->forward(x));
+        x = torch::relu(fc2->forward(x));
+        x = fc3->forward(x);
+
+        return x;
+    }
+
+    torch::nn::Linear fc1{nullptr}, fc2{nullptr}, fc3{nullptr};
+
+}; // end struct Net : torch::nn::Module
 
 int main() {
     // Check if CUDA is available
@@ -26,6 +54,42 @@ int main() {
         torch::Tensor result = tensor + tensor;
         std::cout << "Result of tensor addition on CPU: " << result << std::endl;
     }
+
+
+    std::cout << " \n \t\t THIS is THE GAP \n " << std::endl;
+
+    torch::Device device(torch::kCPU);
+    if (torch::cuda::is_available()) 
+        { device = torch::Device(torch::kCUDA); }
+    
+    Net net;
+    net.to(device);
+
+    CustomLoss custom_loss;
+
+    torch::optim::SGD optimizer(net.parameters(), torch::optim::SGDOptions(0.01));
+
+    auto inputs = torch::rand({10,10}).to(device);
+    auto targets = torch::randn({10,1}).to(device);
+    
+    auto t1 {std::chrono::steady_clock::now()};
+    for (std::size_t epoch = 0; epoch < 10000000; ++epoch)
+    {
+        auto outputs = net.forward(inputs);
+        auto loss = custom_loss.forward(outputs, targets);
+
+
+        std::cout << "Epoch [" << epoch << "]" << " Loss " << loss.item<float>() << std::endl;
+
+
+        optimizer.zero_grad();
+        loss.backward();
+        optimizer.step();
+    }
+    auto t2 {std::chrono::steady_clock::now()};
+    std::cout << std::chrono::duration_cast<std::chrono::microseconds>(t2-t1).count() << std::endl;
+    
+
 
     return 0;
 }
