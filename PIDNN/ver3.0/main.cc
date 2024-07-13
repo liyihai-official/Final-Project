f683d7fc09a820b741373171697f7ec1843f91f2
liyihai-official <pokemonarrive@gmail.com>
2024-07-13 21:08:27 +0800

    Update.PINN

--- PIDNN/ver3.0/main.cc
+++ PIDNN/ver3.0/main.cc
@@ -2,6 +2,8 @@
 #include <iostream>
 #include <chrono>
 
+#include <vector>
+
 struct CustomLoss : torch::nn::Module {
   torch::Tensor forward(  const torch::Tensor & output, 
                           const torch::Tensor & target)
@@ -10,84 +12,193 @@ struct CustomLoss : torch::nn::Module {
 }; // end strct Custom loss : torch::nn::Module
 
 
-struct Net : torch::nn::Module {
-  Net() 
-  {
-    fc1 = register_module("fc1", torch::nn::Linear(10, 500));
-    fc2 = register_module("fc2", torch::nn::Linear(500, 500));
-    fc3 = register_module("fc3", torch::nn::Linear(500, 1));
-  }
+/// @brief This is the module of the customized neural network
+struct Net : torch::nn::Module
+{
+
+  torch::nn::Linear fc1, fc2;
+
+  Net()
+    : fc1(3 , 10),
+      fc2(10, 1)
+    {
+      register_module("fc1", fc1);
+      register_module("fc2", fc2);
+    }
+
 
   torch::Tensor forward(torch::Tensor x)
   {
     x = torch::relu(fc1->forward(x));
-    x = torch::relu(fc2->forward(x));
-    x = fc3->forward(x);
+
+    x = fc2->forward(x);
 
     return x;
   }
 
-  torch::nn::Linear fc1{nullptr}, fc2{nullptr}, fc3{nullptr};
+}; // end struct Net
 
-}; // end struct Net : torch::nn::Module
+class CustomDataset : public torch::data::datasets::Dataset<CustomDataset>
+{
 
-int main() {
-  // Check if CUDA is available
-  if (torch::cuda::is_available()) {
-    std::cout << "CUDA is available! Using GPU." << std::endl;
+  private:
+  std::vector<torch::Tensor> data_;
+  std::vector<torch::Tensor> targets_;
 
-    // Create a tensor and move it to the GPU
-    torch::Tensor tensor = torch::rand({2, 3}).cuda();
-    std::cout << "Tensor on GPU: " << tensor << std::endl;
+  public:
+  CustomDataset( const std::vector<std::vector<float>>& data, const std::vector<float>& targets)
+  {
+    for (const auto& sample: data)
+    {
+      data_.push_back(torch::tensor(sample).view({3}));
+    }
 
-    // Perform a simple operation
-    torch::Tensor result = tensor + tensor;
-    std::cout << "Result of tensor addition on GPU: " << result << std::endl;
-  } else {
-    std::cout << "CUDA is not available. Using CPU." << std::endl;
-
-    // Create a tensor on the CPU
-    torch::Tensor tensor = torch::rand({2, 3});
-    std::cout << "Tensor on CPU: " << tensor << std::endl;
-
-    // Perform a simple operation
-    torch::Tensor result = tensor + tensor;
-    std::cout << "Result of tensor addition on CPU: " << result << std::endl;
+    for (const auto& target : targets)
+    {
+      targets_.push_back(torch::tensor(target).view({1}));
+    }
   }
 
+    torch::data::Example<> get(size_t index) override {
+      return {data_[index], targets_[index]};
+    }
 
-  std::cout << " \n \t\t THIS is THE GAP \n " << std::endl;
+    torch::optional<size_t> size()
+    const override
+    { return data_.size(); }
+    
+};
 
-  torch::Device device(torch::kCPU);
-  if (torch::cuda::is_available()) 
-    { device = torch::Device(torch::kCUDA); }
-  
-  Net net;
-  net.to(device);
 
-  CustomLoss custom_loss;
 
-  torch::optim::SGD optimizer(net.parameters(), torch::optim::SGDOptions(0.01));
+template <typename DataLoader>
+void train(
+  size_t epoch,
+  Net& model,
+  torch::Device device,
+  DataLoader& data_loader,
+  torch::optim::Optimizer& optimizer,
+  size_t dataset_size
+)
+{
+  model.train();
 
-  auto inputs =   torch::rand({10,10}).to(device);
-  auto targets =  torch::randn({10,1}).to(device);
-  
-  auto t1 {std::chrono::steady_clock::now()};
-  for (std::size_t epoch = 0; epoch < 10; ++epoch)
+  size_t batch_idx {0};
+
+  for (auto & batch : data_loader)
   {
-    auto outputs = net.forward(inputs);
-    auto loss = custom_loss.forward(outputs, targets);
-
-
-    std::cout << "Epoch [" << epoch << "]" << " Loss " << loss.item<float>() << std::endl;
-
+    auto data {batch.data.to(device).view({-1,3})}, targets {batch.target.to(device).view({-1,1})};
 
     optimizer.zero_grad();
+
+    auto output { model.forward(data) };
+
+    // std::cout << data.sizes() << "\t" << targets.sizes() << std::endl;
+
+    auto loss {torch::mse_loss(output, targets) };
+
     loss.backward();
+
     optimizer.step();
+
+AT_ASSERT(!std::isnan(loss.template item<float>()));
+
+
+  if (batch_idx++ % 2 == 0)
+    std::printf(
+      "\nTrain Epoch : %ld Loss: %.4f",
+      epoch, loss.template item<float>()
+    );
   }
-  auto t2 {std::chrono::steady_clock::now()};
-  std::cout << std::chrono::duration_cast<std::chrono::microseconds>(t2-t1).count() << std::endl;
+}
+
+
+
+int main() {
+  
+  torch::DeviceType device_type {torch::cuda::is_available() ? torch::kCUDA : torch::kCPU};
+  std::cout << "Running on: " << device_type << std::endl;
+
+  torch::Device device(device_type);
+
+  Net model;
+  model.to(device);
+
+  std::vector<std::vector<float>> data = 
+    {
+      {1.0, 2.0, 3.0},
+      {4.0, 5.0, 0.0},
+      {3.0, 1.0, 1.0},
+      {2.0, 1.0, 1.0}
+    };
+  
+  std::vector<float> targets = {0.1, 0.2, 0.3, 0.1};
+
+
+  auto dataset = CustomDataset(data, targets)
+    .map(torch::data::transforms::Normalize<>(0.0, 1.0))
+    .map(torch::data::transforms::Stack<>());
+
+  const size_t train_dataset_size = dataset.size().value();
+
+  auto data_loader = torch::data::make_data_loader<torch::data::samplers::SequentialSampler>(
+    std::move(dataset),
+    2
+  );
+
+  std::cout << train_dataset_size << std::endl;
+
+  torch::optim::SGD optimizer(
+    model.parameters(), torch::optim::SGDOptions(0.01).momentum(0.5)
+  );
+
+  // for (auto& batch : *data_loader)
+  // {
+  //   auto inputs = batch.data;
+  //   auto targets = batch.target;
+
+  //   std::cout << "Inputs: "   << inputs   << std::endl;
+  //   std::cout << "Targets: "  << targets  << std::endl;
+  //   std::cout << std::endl;
+  // }
+
+  for (size_t epoch = 1; epoch <= 20; ++epoch)
+  {
+    train(epoch, model, device, *data_loader, optimizer, train_dataset_size);
+  }
+  
+  // std::cout << " \n \t\t THIS is THE GAP \n " << std::endl;
+
+  // torch::Device device(torch::kCPU);
+  // if (torch::cuda::is_available()) 
+  //   { device = torch::Device(torch::kCUDA); }
+  
+  // Net net;
+  // net.to(device);
+
+  // CustomLoss custom_loss;
+
+  // torch::optim::SGD optimizer(net.parameters(), torch::optim::SGDOptions(0.01));
+
+  // auto inputs =   torch::rand({10,10}).to(device);
+  // auto targets =  torch::randn({10,1}).to(device);
+  
+  // auto t1 {std::chrono::steady_clock::now()};
+  // for (std::size_t epoch = 0; epoch < 10; ++epoch)
+  // {
+  //   auto outputs = net.forward(inputs);
+  //   auto loss = custom_loss.forward(outputs, targets);
+
+
+  //   std::cout << "Epoch [" << epoch << "]" << " Loss " << loss.item<float>() << std::endl;
+
+
+  //   optimizer.zero_grad();
+  //   loss.backward();
+  //   optimizer.step();
+  // }
+  // auto t2 {std::chrono::steady_clock::now()};
+  // std::cout << std::chrono::duration_cast<std::chrono::microseconds>(t2-t1).count() << std::endl;
 
 
   return 0;
