f6a82f8dabb249e60ae11ce3e7e12e7b1d9f121a
liyihai-official <pokemonarrive@gmail.com>
2024-08-09 19:14:01 +0800

    UPdate.PINN

--- PIDNN/ver3.0/pinn_pde_solver.cpp
+++ PIDNN/ver3.0/pinn_pde_solver.cpp
@@ -24,25 +24,26 @@ using size_type = uint32_t;
 #endif 
 
 
-struct HeatPINN
+struct HeatPINNImpl
   : torch::nn::Module
 {
-  HeatPINN()                  = default;
-  HeatPINN(int ,int, int);
+  HeatPINNImpl()                  = default;
+  HeatPINNImpl(int ,int, int);
   torch::Tensor forward(torch::Tensor);
 
-  torch::nn::Linear input;//, h0, h1, h2, h3, output; 
+  torch::nn::Linear input, h0, output; 
 };
+TORCH_MODULE(HeatPINN);
 
 void get_whole_dataset_X(
   std::vector<Float> &, 
-  std::default_random_engine & , 
+  std::mt19937 & , 
   std::uniform_real_distribution<Float> &, 
   size_type, size_type);
 
 void get_bc_dataset_X(
   std::vector<Float> &, 
-  std::default_random_engine & , 
+  std::mt19937 & , 
   std::uniform_real_distribution<Float> &, 
   size_type, size_type);
 
@@ -63,11 +64,28 @@ int main ()
 
 
   std::cout
-    << "---"
-    << " Create Data Samples"
+    << "---------------------"
+    << " Create Data Samples   // torch::Tensor X_train, Y_train, X; // "
+    << "---------------------"
   << std::endl;
-  std::default_random_engine rde {std::random_device{}()};
+  std::mt19937 rde {std::random_device{}()};
   std::uniform_real_distribution<Float> rng(0.0, 1.0);
+
+
+/// ------------------ Y train ------------------ ///
+
+torch::Tensor Y_train {
+  torch::zeros({2 * (NX + NY, OUT_SIZE)}, device)
+};
+
+#ifndef NDEBUG
+  std::cout 
+    << "-------------------------------------- Y train -------------------------------------- "  << "\n"
+    << "y_train sizes: "            << Y_train.sizes()        << "\n"
+    << "y_train.device().type(): " << Y_train.device().type() << "\n"
+    << "y_train.requires_grad(): " << Y_train.requires_grad() << "\n"
+    << std::endl;
+#endif
 
 /// ------------------ GRID INTERNAL ------------------ ///
   std::vector<Float> X_data;
@@ -80,14 +98,17 @@ int main ()
   };
 
 #ifndef NDEBUG // See the Initialized dataset in the internal grid 
-  std::cout << "X sizes (grid): " << X.sizes()  << "\n"
-            << "X.device().index(): " << X.device().index() << "\n"
-            << "X.requires_grad(): " << X.requires_grad() << "\n"
-            << std::endl;
+  std::cout 
+    << "----------------------------------- GRID INTERNAL -----------------------------------"  << "\n"
+    << "X sizes (grid): "     << X.sizes()                    << "\n"
+    << "X.device().index(): " << X.device().index()           << "\n"
+    << "X.requires_grad(): "  << X.requires_grad()            << "\n"
+    << std::endl;
+
   for (size_type x = 0; x < NX; ++x)
   {
     std::cout << std::fixed << std::setw(3) << x;
-    for (size_type y = 0; y < NY; ++y)
+    for (size_type y = 0; y < 2 * NY; y+=2)
     {
       auto index {x * NY + y};
       std::cout 
@@ -100,18 +121,66 @@ int main ()
   }
   std::cout << std::endl;
 
+
 #endif 
 
+
+/// ------------------ GRID BOUNDARY ------------------ ///
   std::vector<Float> X_train_data;
   get_bc_dataset_X(X_train_data, rde, rng, NX, NY);
 
+  torch::Tensor X_train {
+    torch::from_blob(
+      X_train_data.data(),
+      { 2 * (NX + NY), IN_SIZE }
+    ).to(device)
+  };
+
+#ifndef NDEBUG
+
+  std::cout 
+    << "---------------------------------- GRID BOUNDARY -----------------------------------"  << "\n"
+    << "X_train sizes (grid): "       << X_train.sizes()          << "\n"
+    << "X_train.device().index(): "   << X_train.device().index() << "\n"
+    << "X_train.requires_grad(): "    << X_train.requires_grad()  << "\n"
+    << std::endl;
+
+  for (size_type idx = 0; idx < 2 * 2 * (NX + NY); idx+=2)
+  {
+      std::cout 
+        << std::fixed << std::setw(3) << idx / 2
+        << std::fixed << std::setprecision(3) << std::setw(4)
+        << "["  << X_train_data[idx] 
+        << ", " << X_train_data[idx+1]
+        << "]  "
+        << std::endl;
+  }
+  std::cout << std::endl;
+
+#endif 
 
 
-  // torch::Tensor X_train, Y_train, X;
 
-  // Y_train {
-  //   torch::zeros({})
-  // }
+
+#ifndef NDEBUG 
+  std::cout 
+    << "------------------------------------------"
+    << " Training Process "
+    << "------------------------------------------" << "\n"
+    << std::endl;
+#endif 
+
+  auto net { HeatPINN(IN_SIZE, OUT_SIZE, 2) };
+  net->to(device);
+
+  torch::Tensor loss_sum;
+  Integer iter {1};
+
+  torch::optim::Adam adam_optim(
+    net->parameters(), 
+    torch::optim::AdamOptions(1E-3)
+  );
+
 
   return 0;
 }
@@ -119,7 +188,7 @@ int main ()
 
 void 
 get_whole_dataset_X(std::vector<Float> & obj,
-  std::default_random_engine & rde, 
+  std::mt19937 & rde, 
   std::uniform_real_distribution<Float> & rng,
   size_type nx, size_type ny)
 {
@@ -132,19 +201,43 @@ get_whole_dataset_X(std::vector<Float> & obj,
       obj.push_back(rng(rde));
     }
   }
-
 }
 
 void get_bc_dataset_X(std::vector<Float> & obj,
-  std::default_random_engine & rde, 
+  std::mt19937 & rde, 
   std::uniform_real_distribution<Float> & rng,
   size_type nx, size_type ny)
 {
-  
+
+    for (size_type y = 0; y < ny; ++y)
+    {
+      obj.push_back(0.0);
+      obj.push_back(rng(rde));
+
+      obj.push_back(1.0);
+      obj.push_back(rng(rde));
+    }
+
+    for (size_type x = 0; x < nx; ++x)
+    {
+      obj.push_back(rng(rde));
+      obj.push_back(0.0);
+
+      obj.push_back(rng(rde));
+      obj.push_back(1.0);
+    }
+
 }
 
 
 inline 
-HeatPINN::HeatPINN(int insize, int outsize, int hidsize)
-: input(torch::nn::Linear(insize, hidsize))
-{}
+HeatPINNImpl::HeatPINNImpl(Integer insize, Integer outsize, Integer hidsize)
+: 
+  input(torch::nn::Linear(insize, hidsize)),
+  h0(torch::nn::Linear(hidsize, hidsize)),
+  output(torch::nn::Linear(hidsize, outsize))
+{
+  register_module("Input", input);
+  register_module("hidden_0", h0);
+  register_module("output", output);
+}
