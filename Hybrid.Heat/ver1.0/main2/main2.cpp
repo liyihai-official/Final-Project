390026108f9f4b6fa9396baba31d64a7ece36866
liyihai-official <pokemonarrive@gmail.com>
2024-08-18 21:13:46 +0800

    Update

--- Hybrid.Heat/ver1.0/main2/main2.cpp
+++ Hybrid.Heat/ver1.0/main2/main2.cpp
@@ -1,77 +1,72 @@
 #include <torch/torch.h>
 #include <torch/script.h> // for torch::save
 
-
 #include <numbers>
 #include <random>
 #include <vector>
-#include <cassert>
+
+#include <types.hpp>
 
 #include <pinn/pinn.hpp>
 #include <pinn/dataset.hpp>
 
-using Integer = int;
-using Float   = float;
-
-using size_type = uint32_t;
-
-
-#if !defined (NX) || !defined (NY)
-#define NX 100
-#define NY 100
-#endif 
+/// Problem Size + Boundaries
+#if !defined(NX) || !defined (NY) || !defined(NZ)
+#define NX 200+2
+#define NY 200+2
+#endif
 
 #if !defined (IN_SIZE) || !defined (OUT_SIZE)
 #define IN_SIZE 2
 #define OUT_SIZE 1
 #endif 
 
+/// Using datatypes
+using Integer = final_project::Integer;
+using Char    = final_project::Char;
+using Double  = final_project::Double;
+using Float   = final_project::Float;
+using size_type = final_project::Dworld;
 
-using BCFunction = std::function<Float(Float, Float)>;
+using maintype  = Float;
 
+using BCFunction = std::function<maintype(maintype, maintype)>;
 
 int main ()
 {
+  Integer iter {1};
+  constexpr size_type nsteps {1'000};
+  constexpr size_type numDim {2}, nx {NX}, ny {NY};
 
-  std::cout 
-    << "---" 
-    << " C++ Libtorh Example for Heat Equation 2D PINN"
-    << "\n"          
-    << std::endl;
+  torch::DeviceType dp { torch::cuda::is_available() ? torch::kCUDA : torch::kCPU };
+  torch::Device device { torch::cuda::is_available() ? torch::kCUDA : torch::kCPU };
 
-  torch::DeviceType dp {
-    torch::cuda::is_available() ? torch::kCUDA : torch::kCPU
-  };
-  torch::Device device(dp);
-
-  std::cout << "Running ON: " << dp << std::endl;
+  std::cout << "Problem size: " 
+            << "\n\tRows: "     << nx-2 
+            << "\n\tColumns: "  << ny-2       << std::endl;
+  std::cout << "Running ON: "   << dp         << std::endl;
 
 
-  std::cout
-    << "---------------------"
-    << " Create Data Samples   // torch::Tensor X_train, Y_train, X; // "
-    << "---------------------"
-  << std::endl;
   std::mt19937 rde {std::random_device{}()};
   std::uniform_real_distribution<Float> rng(0.0, 1.0);
 
-  BCFunction Dim00 {[](Float x, Float y){ return 5 * std::abs(std::sin(y * 2 * std::numbers::pi));}};
-  BCFunction Dim01 {[](Float x, Float y){ return 0;}};
+  BCFunction Dim00 {[](maintype x, maintype y){ return y;}};
+  BCFunction Dim01 {[](maintype x, maintype y){ return 1;}};
 
-  BCFunction Dim10 {[](Float x, Float y){ return 20 * std::sin(x * 2 * std::numbers::pi);}};
-  BCFunction Dim11 {[](Float x, Float y){ return -20  * std::sin(x * 2 * std::numbers::pi);}};
+  BCFunction Dim10 {[](maintype x, maintype y){ return x;}};
+  BCFunction Dim11 {[](maintype x, maintype y){ return 1;}};
+
 
   final_project::PINN::dataset dataset (IN_SIZE, OUT_SIZE, NX, NY, rde, rng, Dim00, Dim01, Dim10, Dim11, device);
 
-  dataset.show_Y_boundary();
-  dataset.show_X_internal();
-  dataset.show_X_boundary();
+  // dataset.show_Y_boundary();
+  // dataset.show_X_internal();
+  // dataset.show_X_boundary();
 
   auto net { final_project::PINN::HeatPINN(IN_SIZE, OUT_SIZE, /*hsize*/ 20) };
   net->to(device);
 
   torch::Tensor loss_sum;
-  Integer iter {1}, nsteps {20'000};
 
   torch::optim::Adam adam_optim(
     net->parameters(), 
@@ -90,14 +85,14 @@ int main ()
 
     adam_optim.step(closure);
 
-    if (iter % 300 == 0)
-    {
-      std::cout 
-        << "Iteration = " << iter << "\t"
-        << "Loss = " << std::fixed << std::setprecision(4) << std::setw(8) << loss_sum.item<Float>() << "\t"
-        << "Loss.Device.Type = " << loss_sum.device().type() 
-        << std::endl; 
-    }
+    // if (iter % 300 == 0)
+    // {
+    //   std::cout 
+    //     << "Iteration = " << iter << "\t"
+    //     << "Loss = " << std::fixed << std::setprecision(4) << std::setw(8) << loss_sum.item<Float>() << "\t"
+    //     << "Loss.Device.Type = " << loss_sum.device().type() 
+    //     << std::endl; 
+    // }
 
     ++iter;
     if (loss_sum.item<Float>() < 1E-2) break;
@@ -105,32 +100,32 @@ int main ()
 
 
 
-  std::cout 
-    << "Training stopped." << "\n"
-    << "Final iter=" << iter - 1 << "\n"
-    << "loss=" << std::setprecision(7) << loss_sum.item<Float>()  << "\n"
-    << "loss.device().type()=" << loss_sum.device().type() 
-    << std::endl;
+  // std::cout 
+  //   << "Training stopped." << "\n"
+  //   << "Final iter=" << iter - 1 << "\n"
+  //   << "loss=" << std::setprecision(7) << loss_sum.item<Float>()  << "\n"
+  //   << "loss.device().type()=" << loss_sum.device().type() 
+  //   << std::endl;
 
-  torch::save(net, "model.pt");
+  // torch::save(net, "model.pt");
 
 
-  final_project::PINN::dataset valset (IN_SIZE, OUT_SIZE, NX, NY, device);
-  auto out = net->forward(valset.X_internal);
+  // final_project::PINN::dataset valset (IN_SIZE, OUT_SIZE, NX, NY, device);
+  // auto out = net->forward(valset.X_internal);
   // std::cout << out << std::endl;
 
-  final_project::multi_array::array_base<Float, 2> gather (NX, NY);
+  // final_project::multi_array::array_base<Float, 2> gather (NX, NY);
 
-  for (Integer x = 0; x < NX; ++x)
-  {
-    for (Integer y = 0; y < NY; ++y)
-    {
-      gather(x,y) = out.index({x * NY + y}).item<Float>();
-    }
-  }
+  // for (Integer x = 0; x < NX; ++x)
+  // {
+  //   for (Integer y = 0; y < NY; ++y)
+  //   {
+  //     gather(x,y) = out.index({x * NY + y}).item<Float>();
+  //   }
+  // }
 
-  std::cout << gather.data() << std::endl;
-  gather.saveToBinary("test.bin");
+  // std::cout << gather.data() << std::endl;
+  // gather.saveToBinary("test.bin");
 
   return 0;
 
